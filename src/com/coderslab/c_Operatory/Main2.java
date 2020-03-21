d9abdc7dfcf6d10194a59a3811dde8376997b264
sswietlik <sswietlik@gmail.com>
2020-03-21 13:13:35 +0100

    Powtorka

--- src/com/coderslab/c_Operatory/Main2.java
+++ src/com/coderslab/c_Operatory/Main2.java
@@ -3,7 +3,26 @@ package com.coderslab.c_Operatory;
 public class Main2 {
 
     public static void main(String[] args) {
+        int nr1 = 10;
+        int nr2 = 4;
+        int resultModulo = 0;
+
+
+        System.out.println(nr1 % nr2);
+        resultModulo = (nr1 % nr2);
+        System.out.println(resultModulo);
+
+
+
 
     }
 
 }
+
+//
+//    W pliku `Main2.java` stwórz trzy zmienne o nazwach: `nr1`, `nr2`, `resultModulo`.
+//
+//        1. Dwie pierwsze niech przechowują dowolne liczby całkowite.
+//        2. Trzecia – o nazwie ``resultModulo`` – niech przechowuje liczbę 0.
+//        3. Oblicz resztę z dzielenia **(modulo)** tych liczb (**nr1 modulo nr2**) i zapisz wynik w zmiennej ```resultModulo```.
+//        4. Wypisz zmienną ``resultModulo``.
