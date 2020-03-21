07d914ff61f201dc53848d6b54daeda52d42ef44
sswietlik <sswietlik@gmail.com>
2020-03-21 13:05:55 +0100

    Powtorka

--- src/com/coderslab/c_Operatory/Main1.java
+++ src/com/coderslab/c_Operatory/Main1.java
@@ -4,24 +4,28 @@ public class Main1 {
 
     public static void main(String[] args) {
 
-        int first = 11;
+        boolean first = (1 <= 2);
         System.out.println(first);
-        int second = 33;
+        boolean second = (10<5);
         System.out.println(second);
-        int isFirstEqualSecond = 0;
-        System.out.println(isFirstEqualSecond + " before overwrt");
 
-        isFirstEqualSecond = (first == second)
+
+        boolean isFirstEqualSecond = (first == second);
+        System.out.println(isFirstEqualSecond);
+
+
+
+
 
     }
 
 }
 
-//#### Zadanie 1
+// Zadanie 1
 //
-//        W pliku `Main1.java` stwórz dwie zmienne o nazwach: `first`, `second`.
+//         W pliku `Main1.java` stwórz dwie zmienne o nazwach: `first`, `second`.
 //
-//        1. Przypisz do nich dwie **różne** wartości logiczne jakie znasz.
-//        2. Porównaj je za pomocą operatora ```==```, a wynik porównania zapisz do trzeciej zmiennej – `isFirstEqualSecond`.
-//        3. Wyświetl w konsoli zmienną `isFirstEqualSecond`, aby sprawdzić wynik.
-//        4. Zmodyfikuj wartość zmiennej `second` na przeciwną i ponownie sprawdź wynik.
+//         1. Przypisz do nich dwie **różne** wartości logiczne jakie znasz.
+//         2. Porównaj je za pomocą operatora ```==```, a wynik porównania zapisz do trzeciej zmiennej – `isFirstEqualSecond`.
+//         3. Wyświetl w konsoli zmienną `isFirstEqualSecond`, aby sprawdzić wynik.
+//         4. Zmodyfikuj wartość zmiennej `second` na przeciwną i ponownie sprawdź wynik.
