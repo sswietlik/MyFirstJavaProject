0c2482c539522c2475c004c43186d309f146f568
sswietlik <sswietlik@gmail.com>
2020-03-21 18:48:37 +0100

    kontrola przeplywu - if/ else

--- src/com/coderslab/d_Kontrola_Przeplywu_programu/Main1.java
+++ src/com/coderslab/d_Kontrola_Przeplywu_programu/Main1.java
@@ -2,6 +2,23 @@ package com.coderslab.d_Kontrola_Przeplywu_programu;
 
 public class Main1 {
     public static void main(String[] args) {
+        int nr1 = 1;
+        int nr2 = 2;
+
+        if (nr2 > nr1){
+            System.out.println(nr2 + " is bigger then " + nr1);
+        } else if (nr1 > nr2){
+            System.out.println(nr1 + " is bigger then " + nr2);
+        } else {
+            System.out.println("...");
+        }
+
 
     }
 }
+
+//#### Zadanie 1
+//
+//        1. W pliku `Main1.java` stwórz dwie zmienne, o nazwach: `nr1, nr2`, przechowujące liczby całkowite.
+//        2. Następnie za pomocą instrukcji warunkowej ```if ... else ```, wypisz w konsoli, która z nich jest większa.
+//        3. Wypisany String ma być wg wzoru: `Większa liczba to 7.`
